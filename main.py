79842587f29fe7f43952691bc2dd1aa7d09e564e
anas.kambal <kambal.anas@gmail.com>
2022-08-13 12:35:20 +0200

    threadded sensor posts

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
+import threading
+import time
 from wsgiref.simple_server import make_server
 import falcon
+import requests
 #import serial
 
 #ser = serial.Serial(
@@ -23,13 +26,14 @@ import falcon
 
 def randomSensorData(min, max):
     n = random.randint(min, max)
-    return  n
+    return n
 
 
 
 class helpPage:
 
     def on_get(self, req, resp):
+        print(threading.current_thread().name)
         resp.content_type = falcon.MEDIA_TEXT
         resp.text = ('Hello! Following endpoints are avalialbe:\n\n' +
                      req.url + '<path>\n\n'
@@ -75,6 +79,7 @@ class getTemperature:
     def on_get(self, req, resp):
         #message = req.media.get("message")
         print('I was called on getTemp\n')
+        print(threading.current_thread().name)
         currentTemp = {
             'Temp': (randomSensorData(0,35))
         }
@@ -86,6 +91,7 @@ class getBatteryLevel:
 
     def on_get(self, req, resp):
         print('I was called on getBatteryLevel\n')
+        print(threading.current_thread().name)
         currentBatteryLvl = {
            'Battery': (randomSensorData(0, 100))
         }
@@ -97,6 +103,7 @@ class getSpeed:
 
     def on_get(self, req, resp):
         print('I was called on getSpeed\n')
+        print(threading.current_thread().name)
         currentSpeed = {
            'Speed': (randomSensorData(0, 10))
         }
@@ -109,6 +116,7 @@ class getGyrosensor:
 
     def on_get(self, req, resp):
         print('I was called on getGyro\n')
+        print(threading.current_thread().name)
         currentGyro = {
            'Gyro': ({
                'x': randomSensorData(-10, 10),
@@ -120,6 +128,27 @@ class getGyrosensor:
         resp.media = currentGyro
         resp.status = falcon.HTTP_200
 
+def longTask():
+    print("started bg task..")
+    print(threading.current_thread().name)
+    while(1):
+        speed = randomSensorData(0, 10)
+        temp = randomSensorData(0, 40)
+        battery = randomSensorData(0, 100)
+        requests.post("http://localhost:3002/receiveGyro", json=[{"x": 0.1, "y": 0.2, "z": 0.3}])
+        time.sleep(0.5)
+        requests.post("http://localhost:3002/receiveGyro", json=[{"x": 0.15, "y": 0.15, "z": 0.25}])
+        time.sleep(0.5)
+        requests.post("http://localhost:3002/receiveGyro", json=[{"x": -0.13, "y": -0.13, "z": 0.2}])
+        time.sleep(0.5)
+        requests.post("http://localhost:3002/receiveGyro", json=[{"x": -0.11, "y": -0.12, "z": 0.2}])
+        time.sleep(0.5)
+        requests.post("http://localhost:3002/receiveGyro", json=[{"x": -0.10, "y": -0.11, "z": 0.2}])
+        time.sleep(0.5)
+        requests.post("http://localhost:3002/receiveGyro", json=[{"x": -0.9, "y": -0.1, "z": 0.2}])
+        time.sleep(0.5)
+        requests.post("http://localhost:3002/receiveSensorData", json={"tmp": temp, "speed": speed, "battery": battery})
+        time.sleep(1)
 
 app = falcon.App(middleware=falcon.CORSMiddleware(allow_origins='*', allow_credentials='*'))
 help_page = helpPage()
@@ -139,6 +168,7 @@ app.add_route('/speed', getSpd)
 app.add_route('/gyro', getGyro)
 
 if __name__ == '__main__':
+    threading.Thread(target=longTask).start()
     with make_server('', 80, app) as httpd:
         print('Server is listening on Port 80 ...')
         httpd.serve_forever()
